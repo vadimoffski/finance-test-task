0100ded8c40edce6f5a38bb0f670fa48d321a49a
Vadim <hryshchenko95@gmail.com>
2022-08-24 19:56:18 +0300

    implemented socket.io and cleared App.css

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
+/* eslint-disable react-hooks/exhaustive-deps */
 import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { fulfilled, pending, rejected } from './redux/app/tickerSlice';
+import { io } from "socket.io-client";
 
-function App() {
+
+const App = () => {
+  const socket = io(`${process.env.REACT_APP_WS_URI}`);
+  const dispatch = useDispatch()
+  const store = useSelector((store) => store.ticker)
+  console.log(store);
+
+  useEffect(() => {
+    try {
+      dispatch(pending())
+      if (socket.connected) {
+        dispatch(rejected(`Server error!`))
+      }
+      socket.emit('start')
+      socket.on('ticker', (response) => {
+        const res = Array.isArray(response) ? response : [response]
+        dispatch(fulfilled(res))
+      })
+    }
+    catch (error) {
+      dispatch(rejected(error))
+    }
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <><h2>hello</h2></>
   );
 }
 
