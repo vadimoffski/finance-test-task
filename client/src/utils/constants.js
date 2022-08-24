4bc5d05463be356e11b8b2ca36119ecb67ea533c
Vadim <hryshchenko95@gmail.com>
2022-08-25 01:43:52 +0300

    added constants

---
+++ client/src/utils/constants.js
@@ -0,0 +1,10 @@
+
+export const companies = {
+  AAPL: "Apple",
+  GOOGL: "Alphabet",
+  MSFT: "Microsoft",
+  AMZN: "Amazon",
+  FB: "Facebook",
+  TSLA: "Tesla",
+};
+export const cellsTitle = ['Company', 'Ticker', 'Price', 'Change', 'Change Percent', 'Dividend', 'Yield', 'Last trade time']
