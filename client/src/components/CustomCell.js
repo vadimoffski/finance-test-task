05ba388cef87170bbab6d0810606e72c1d1fd26f
Vadim <hryshchenko95@gmail.com>
2022-08-25 01:47:05 +0300

    created CustomCell component

---
+++ client/src/components/CustomCell.js
@@ -0,0 +1,36 @@
+import React, { useRef } from 'react'
+import TableCell from '@mui/material/TableCell';
+import { makeStyles } from '@mui/styles';
+import { isTrend } from '../utils/helpers'
+
+const useStyles = makeStyles({
+  increment: {
+    backgroundColor: 'green',
+    width: '50px',
+    color: 'white'
+  },
+  decrement: {
+    backgroundColor: 'red',
+    width: '50px',
+    color: 'white'
+  }
+});
+
+const CustomCell = ({ title, ...props }) => {
+  const classes = useStyles();
+  const prev = useRef(null)
+
+  return (
+    <>
+      <TableCell
+        className={`${isTrend(prev?.current?.innerText, title) ? classes.increment : classes.decrement}`}
+        ref={prev}
+        {...props}
+      >
+        {`${title} ${isTrend(prev?.current?.innerText, title) ? "↑" : "↓"}`}
+      </TableCell>
+    </>
+  )
+}
+
+export default React.memo(CustomCell)
